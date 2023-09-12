05b82a346c3e76bb565ecb43fa0af694f17f6043
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2023-09-12 10:54:20 +0700

    Add migration example

--- src/databases/postgres.go
+++ src/databases/postgres.go
@@ -20,7 +20,7 @@ func NewPostgresClient() (*gorm.DB, error) {
 		return nil, err
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Bangkok",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USERNAME"),
 		os.Getenv("DB_PASSWORD"),
