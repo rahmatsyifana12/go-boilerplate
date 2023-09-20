726d8b22fdd1e56256da515db257f0d1744df649
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2023-09-21 00:23:31 +0700

    Add access_token into users table

--- migrations/20230912033821_create-user-table.up.sql
+++ migrations/20230912033821_create-user-table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
    id serial PRIMARY KEY,
    username VARCHAR(255) UNIQUE NOT NULL,
    password VARCHAR(255) NOT NULL,
+   access_token VARCHAR(255),
    created_at TIMESTAMP NOT NULL,
    updated_at TIMESTAMP,
    deleted_at TIMESTAMP
--- src/models/user_model.go
+++ src/models/user_model.go
@@ -8,5 +8,6 @@ type User struct {
 	gorm.Model
 	Username    string  `json:"username"`
 	Password    string  `json:"password"`
+	AccessToken string  `json:"access_token"`
     Todos       []Todo
 }
