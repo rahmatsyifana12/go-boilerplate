0715bd6d9895df262070fa289aec192b9c812b17
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2023-12-03 18:33:26 +0700

    Add logger auto create folder

--- src/main.go
+++ src/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"go-boilerplate/src/logger"
+	"go-boilerplate/src/middlewares"
 	"os"
 	"strings"
+
 	echo_middlewares "github.com/labstack/echo/v4/middleware"
 
 	"github.com/labstack/echo/v4"
@@ -12,9 +15,14 @@ import (
 func main() {
     e := echo.New()
 
+	if err := logger.SetupLogger(); err != nil {
+		panic("Failed to setup logger with error: " + err.Error())
+	}
+
 	e.Use(echo_middlewares.CORSWithConfig(echo_middlewares.CORSConfig{
 		AllowOrigins: strings.Split(os.Getenv("CORS_ALLOWED_ORIGINS"), ","),
 	}))
+	e.Use(middlewares.Log)
 
 	module := Module{}
 	module.New(e)
